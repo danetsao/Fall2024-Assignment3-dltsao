57c1fe11a5feb8c0488a28d44f949452e53e254e
danetsaoo <danetsaoo@gmail.com>
2024-10-18 17:25:17 -0500

    Init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fall2024_Assignment3_dltsao.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
